a84374c89fe509f262199cb640c66be21800bb5e
Jubel <jubelcassioverridasilva@gmail.com>
2021-06-25 19:49:19 -0300

    f

--- tests/test_a.py
+++ tests/test_a.py
@@ -2,6 +2,6 @@ class TestSuccess:
     def test_success(self):
         assert True
 
-class TestFailure:
-    def test_failure(self):
-        assert False
+# class TestFailure:
+#     def test_failure(self):
+#         assert False
